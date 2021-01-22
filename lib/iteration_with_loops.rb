a9493c51b6f2f556973f5ec2baccd065d0216349
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-22 15:58:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,21 +16,21 @@
     #smallest_array << smallest_num
     #row_index += 1
 #end
-#smallest_array
-#end
-
-def find_min_in_nested_arrays(src)
- smallest_array = []
-  row_index = 0 
-  while row_index < src.length do
-    element_index = 0 
-    while element_index < src[row_index].length do
-        src[row_index][element_index..].min << smallest_array
-     element_index += 1
-     end
-   
-    row_index += 1
-end
-
 smallest_array
 end
+
+#def find_min_in_nested_arrays(src)
+ #smallest_array = []
+  #row_index = 0 
+  #while row_index < src.length do
+    #element_index = 0 
+    #while element_index < src[row_index].length do
+     #   src[row_index][element_index..].min << smallest_array
+     #element_index += 1
+     #end
+   
+    #row_index += 1
+#end
+
+#smallest_array
+#end
